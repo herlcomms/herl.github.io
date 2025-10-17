588f2b7e8e4217f2b425b85dc9c1174e2205993c
herlcomms <herl@groups.pitt.edu>
2025-10-17 11:06:30 -0400

    Update viewer.js

--- pdfjs/viewer.js
+++ pdfjs/viewer.js
@@ -52,7 +52,6 @@ var MAX_AUTO_SCALE = 1.25;
 var SCROLLBAR_PADDING = 40;
 var VERTICAL_PADDING = 5;
 
-
 // optimised CSS custom property getter/setter
 var CustomStyle = (function CustomStyleClosure() {
 
@@ -2280,30 +2279,10 @@ var SecondaryToolbar = {
   },
 
   // Event handling functions.
-  //presentationModeClick: function secondaryToolbarPresentationModeClick(evt) {
-  //  PDFViewerApplication.requestPresentationMode();
-  //  this.close();
-  //},
-
-    // Event handling functions.
-
-  presentationModeClick:   function togglePageFullscreen() {
-      var magazine = document.getElementById("magiframe");
-  if (magazine.fullscreenElement) {
-    magazine.exitFullscreen();
-  } else {
-    if (magazine.documentElement.requestFullscreen) {
-      magazine.documentElement.requestFullscreen();
-    } else if (magazine.documentElement.mozRequestFullScreen) {
-      magazine.documentElement.mozRequestFullScreen();
-    } else if (magazine.documentElement.webkitRequestFullscreen) {
-      magazine.documentElement.webkitRequestFullscreen();
-    } else if (magazine.documentElement.msRequestFullscreen) {
-      magazine.documentElement.msRequestFullscreen();
-    }
-  }
-},
-
+  presentationModeClick: function secondaryToolbarPresentationModeClick(evt) {
+    PDFViewerApplication.requestPresentationMode();
+    this.close();
+  },
 
   openFileClick: function secondaryToolbarOpenFileClick(evt) {
     document.getElementById('fileInput').click();
